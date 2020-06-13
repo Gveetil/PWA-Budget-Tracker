2069009120b913c84122e28ef70d4914404d54da
Geethanjali Veetil <gtveetil@gmail.com>
2020-06-13 16:35:49 +1000

    Update icons and colors

--- webpack.config.js
+++ webpack.config.js
@@ -28,12 +28,12 @@ const config = {
       short_name: "Budget",
       description: "Budget Tracker keeps track of your Budget, with or without an internet connection.",
       background_color: "#dddddd",
-      theme_color: "#ffffff",
-      "theme-color": "#ffffff",
+      theme_color: "#317EFB",
+      "theme-color": "#317EFB",
       fingerprints: false,
       start_url: "/",
       icons: [{
-        src: path.resolve(__dirname, "public/assets/icons/icon_192x192.png"),
+        src: path.resolve(__dirname, "public/assets/icons/icon-512x512.png"),
         sizes: [96, 128, 192, 256, 384, 512],
         destination: path.join("assets", "icons")
       }]
